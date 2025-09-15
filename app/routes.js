e8f920b10242c35f07bba4039f5e1258161c87f7
chrissiebradley <chrissie.bradley1@nhs.net>
2025-09-15 13:47:13 +0000

    added routing from magical powers page

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,29 @@ const express = require('express')
 
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+// Called when answering question about whether NHS number is known
+router.post('/magical-powers-answers', (req, res) => {
+
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const hasSymptoms = req.session.data.hasSymptoms
+
+  // Check whether the variable matches a condition
+  if (hasSymptoms === 'Yes') {
+
+    // Send user to a page where they’ll enter their NHS number
+    res.redirect('/details')
+
+  } else if (hasSymptoms === 'No') {
+
+    // Send user to a page where they can find their NHS number
+    res.redirect('/ineligible')
+
+  } else {
+
+    // Send user back to the question page
+    res.redirect('/details')
+
+  }
+})
 
 module.exports = router
